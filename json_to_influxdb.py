9f4a4796980e3ce70566fcbc856f49407cb7c3ee
GuillermoElectrico <guillermoelectrico@outlook.com>
2019-02-24 13:44:47 +0100

    fist commit

---
+++ json_to_influxdb.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+from influxdb import InfluxDBClient
+from datetime import datetime, timedelta
+from os import path
+import sys
+import os
+import time
+import yaml
+import logging
+import subprocess
+import json
+
+# Change working dir to the same dir as this script
+os.chdir(sys.path[0])
+
+class DataCollector:
+    def __init__(self, influx_yaml):
+        self.influx_yaml = influx_yaml
+        self.influx_map = None
+        self.influx_map_last_change = -1
+        log.info('InfluxDB:')
+        for influx_config in sorted(self.get_influxdb(), key=lambda x:sorted(x.keys())):
+            log.info('\t {} <--> {}'.format(influx_config['host'], influx_config['name']))
+
+    def get_influxdb(self):
+        assert path.exists(self.influx_yaml), 'InfluxDB map not found: %s' % self.influx_yaml
+        if path.getmtime(self.influx_yaml) != self.influx_map_last_change:
+            try:
+                log.info('Reloading influxDB map as file changed')
+                new_map = yaml.load(open(self.influx_yaml))
+                self.influx_map = new_map['influxdb']
+                self.influx_map_last_change = path.getmtime(self.influx_yaml)
+            except Exception as e:
+                log.warning('Failed to re-load influxDB map, going on with the old one.')
+                log.warning(e)
+        return self.influx_map
+       
+
+    def read_and_store(self, filelog_json):
+        influxdb = self.get_influxdb()
+        
+        log.info('Open file as json {}' .format(filelog_json))
+        
+        with open(filelog_json, 'r') as json_file:
+            json_body = json.loads(json_file)
+        
+            if len(json_body) > 0:
+
+                #log.debug(json_body)
+
+                for influx_config in influxdb:
+
+                    DBclient = InfluxDBClient(influx_config['host'],
+                                            influx_config['port'],
+                                            influx_config['user'],
+                                            influx_config['password'],
+                                            influx_config['dbname'])
+                    try:
+                        DBclient.write_points(json_body)
+                        log.info(t_str + ' Data written for %d records.' % len(json_body) )
+                    except Exception as e:
+                        log.error('Data not written! in {}' .format(influx_config['name']))
+                        log.error(e)
+                        raise
+            else:
+                log.warning(t_str, 'No data sent.')
+
+            
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filelog', default="localhost", help='file containing log in json format. Default "log00000.log"')
+    parser.add_argument('--influxdb', default='influx_config.yml',
+                        help='YAML file containing Influx Host, port, user etc. Default "influx_config.yml"')
+    parser.add_argument('--log', default='CRITICAL',
+                        help='Log levels, DEBUG, INFO, WARNING, ERROR or CRITICAL')
+    parser.add_argument('--logfile', default='',
+                        help='Specify log file, if not specified the log is streamed to console')
+    args = parser.parse_args()
+    loglevel = args.log.upper()
+    logfile = args.logfile
+
+    # Setup logging
+    log = logging.getLogger('mqtt2influx-logger')
+    log.setLevel(getattr(logging, loglevel))
+
+    if logfile:
+        loghandle = logging.FileHandler(logfile, 'w')
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        loghandle.setFormatter(formatter)
+    else:
+        loghandle = logging.StreamHandler()
+
+    log.addHandler(loghandle)
+
+    log.info('Started app')
+    
+    collector = DataCollector(influx_yaml=args.influxdb)
+    
+    collector.read_and_store(filelog_json=args.filelog)
+
+    log.info('End app')
+
+    
+
+
